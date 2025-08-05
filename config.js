21c56ead70e8fe0ba657e7857f9d01aec8bea33b
miyurugamage1234 <mggamage9@gmail.com>
2025-08-06 00:37:41 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,8 +8,8 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
 
 SESSION_ID: process.env.SESSION_ID === undefined ? 'PUT SESSION ID HERE' : process.env.SESSION_ID, 
-PORT: process.env.PORT === undefined ? '8000' : process.env.PORT,    
-SESSION_NAME: process.env.SESSION_NAME === undefined ? 'vispermd' : process.env.SESSION_NAME, 
+PORT: process.env.PORT === undefined ? 'ZAAysBfRPcz2ymd6mGKngJN4QszYXJ3DTFwa' : process.env.PORT,    
+SESSION_NAME: process.env.SESSION_NAME === undefined ? 'miyurugamage1234' : process.env.SESSION_NAME, 
 
 };
 
